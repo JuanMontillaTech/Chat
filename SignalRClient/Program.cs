0599d2e67c9c165efc496913057ff7ecaa5439f9
Juan Ramon Montilla Sanchez <ing.juan.montilla@gmail.com>
2020-06-05 21:31:06 -0400

    Chat
    Modelo de chat

---
+++ SignalRClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/ChatHub")
+                .Build();
+            connection.On<string>("Send", (message) =>
+            {
+                Console.WriteLine("Recibido:");
+                Console.WriteLine(message);
+            });
+            connection.StartAsync().Wait();
+            while (true)
+            {
+                Console.WriteLine("Escriba un mensaje:");
+                var message = Console.ReadLine();
+                connection.SendAsync("Send", message);
+            }
+        }
+    }
+}
---
+++ SingalRHub/Chat.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+
+namespace SingalRHub
+{
+    public class Chat :Hub
+    {
+        public Task send (string message)
+        {
+            System.Console.WriteLine(message);
+            return Clients.Others.SendCoreAsync("Send", new[] { message });
+        }
+    }
+}
